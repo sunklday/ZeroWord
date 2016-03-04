70f078825157bb06d7523754fbd65e727ec96a37
admin <446853122@qq.com>
2016-03-04 19:12:41 +0800

    添加了数据库。明天搞随机选单词。

--- app/src/main/java/sunkl/jiai/com/zeroword/activity/ConfigActivity.java
+++ app/src/main/java/sunkl/jiai/com/zeroword/activity/ConfigActivity.java
@@ -2,14 +2,56 @@ package sunkl.jiai.com.zeroword.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import sunkl.jiai.com.zeroword.R;
+import sunkl.jiai.com.zeroword.db.DBManager;
 
 public class ConfigActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
+        button  = (Button) findViewById(R.id.btn_adddb);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputStream inputStream = getResources().openRawResource(R.raw.sijiword3);
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                DBManager dbManager =new DBManager(ConfigActivity.this);
+                String line ;
+                int i=0;
+                try {
+                    while(i<500) {
+                        i++;
+                        line=bufferedReader.readLine();
+                        String[] str = line.split("\\*",3);
+                        if(str.length<2)
+                        {
+                            continue;
+                        }
+                        dbManager.insert(str[0],str[1],str[2],0);
+                        for(int j=0;j<str.length;j++){
+                            System.out.println(str[j]+" " +j);
+                        }
+                    }
+                    if(i>498) {
+                        Toast.makeText(getApplicationContext(), "添加成功",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } catch (IOException e){
+
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/sunkl/jiai/com/zeroword/db/DBManager.java
+++ app/src/main/java/sunkl/jiai/com/zeroword/db/DBManager.java
@@ -12,24 +12,43 @@ import android.provider.BaseColumns;
  */
 public class DBManager extends SQLiteOpenHelper {
 
+
     /**
      * 常规字段
+     * text integer 数据库数据类型
+     * 逗号是分隔符
      */
     private static final String TEXT_TYPE = " TEXT";
+    private static final String INTEGER_TYPE = " INTEGER";
     private static final String COMMA_SEP = ",";
-
+    /**
+     * 建立user表sql语句
+     */
+    private static final String SQL_CREATE_USER =
+            "CREATE TABLE " + USERTABLE.TABLE_NAME + " (" +
+                    USERTABLE._ID + " INTEGER PRIMARY KEY," +
+                    USERTABLE.COLUMN_NAME_NAME + TEXT_TYPE + COMMA_SEP +
+                    USERTABLE.COLUMN_NAME_TIME + INTEGER_TYPE +COMMA_SEP+
+                    USERTABLE.COLUMN_NAME_WORDMARK + TEXT_TYPE +COMMA_SEP+
+                    USERTABLE.COLUMN_NAME_AMOUNT + INTEGER_TYPE +
+                    " )";
+    /**
+     * 建立word表sql语句
+     */
     private static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
-                    FeedEntry._ID + " INTEGER PRIMARY KEY," +
-                    FeedEntry.COLUMN_NAME_ENTRY_ID + TEXT_TYPE + COMMA_SEP +
-                    FeedEntry.COLUMN_NAME_WORD + TEXT_TYPE + COMMA_SEP +
-                    FeedEntry.COLUMN_NAME_MEAN + TEXT_TYPE +
+            "CREATE TABLE " + WORDTABLE.TABLE_NAME + " (" +
+                    WORDTABLE._ID + " INTEGER PRIMARY KEY," +
+                    WORDTABLE.COLUMN_NAME_ENTRY_ID + TEXT_TYPE + COMMA_SEP +
+                    WORDTABLE.COLUMN_NAME_WORD + TEXT_TYPE + COMMA_SEP +
+                    WORDTABLE.COLUMN_NAME_MEAN + TEXT_TYPE + COMMA_SEP +
+                    WORDTABLE.COLUMN_NAME_EXAMPLE + TEXT_TYPE + COMMA_SEP +
+                    WORDTABLE.COLUMN_NAME_DEGREE + INTEGER_TYPE +
                     " )";
 
     private static final String SQL_DELETE_ENTRIES =
-            "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
+            "DROP TABLE IF EXISTS " + WORDTABLE.TABLE_NAME;
 
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
     public static final String DATABASE_NAME = "word.db";
 
     public DBManager(Context context) {
@@ -37,43 +56,76 @@ public class DBManager extends SQLiteOpenHelper {
     }
 
     /**
-     * 定义表结构
+     * 定义word表结构
+     * word 单词
+     * id 单词的id 暂时不用
+     * mean 释义
+     * example 例句
+     * degree 单词的程度 用来标记单词难度或者复习程度
+     *
      */
-    public static abstract class FeedEntry implements BaseColumns {
+    public static abstract class WORDTABLE implements BaseColumns {
         public static final String TABLE_NAME = "word";
         public static final String COLUMN_NAME_ENTRY_ID = "id";
         public static final String COLUMN_NAME_WORD = "word";
         public static final String COLUMN_NAME_MEAN = "mean";
+        public static final String COLUMN_NAME_EXAMPLE = "example";
+        public static final String COLUMN_NAME_DEGREE = "degree";
 
     }
 
+    /**
+     * 定义user表结构
+     * user 用户名字
+     * time 倒计时用来闹钟
+     * wordmark 标记当前背单词的那个单词
+     * amount 设置每日背单词的数量
+     */
+    public static abstract class USERTABLE implements BaseColumns {
+        public static final String TABLE_NAME = "user";
+        public static final String COLUMN_NAME_NAME = "name";
+        public static final String COLUMN_NAME_TIME = "time";
+        public static final String COLUMN_NAME_WORDMARK = "mean";
+        public static final String COLUMN_NAME_AMOUNT = "amount";
+    }
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(SQL_CREATE_ENTRIES);
+        db.execSQL(SQL_CREATE_USER);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // This database is only a cache for online data, so its upgrade policy is
         // to simply to discard the data and start over
-        db.execSQL(SQL_DELETE_ENTRIES);
-        onCreate(db);
+        switch (oldVersion){
+            case 1:
+                onCreate(db);
+            case 2:
+                onCreate(db);
+            default:
+                break;
+        }
+
     }
 
     /**
      * 插入
+     *
      * @param word 单词
      * @param mean 释义
      * @return
      */
-    public long insert(String word,String mean){
+    public long insert(String word, String mean, String example, int degree) {
         SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues cv=new ContentValues();
-        cv.put(FeedEntry.COLUMN_NAME_WORD, word);
-        cv.put(FeedEntry.COLUMN_NAME_MEAN, mean);
+        ContentValues cv = new ContentValues();
+        cv.put(WORDTABLE.COLUMN_NAME_WORD, word);
+        cv.put(WORDTABLE.COLUMN_NAME_MEAN, mean);
+        cv.put(WORDTABLE.COLUMN_NAME_EXAMPLE, example);
+        cv.put(WORDTABLE.COLUMN_NAME_DEGREE, degree);
         long newRowId;
         newRowId = db.insert(
-                FeedEntry.TABLE_NAME,
+                WORDTABLE.TABLE_NAME,
                 null,
                 cv);
         return newRowId;
@@ -81,28 +133,95 @@ public class DBManager extends SQLiteOpenHelper {
 
     /**
      * sql select *
+     * query(table, columns, selection, selectionArgs, groupBy, having, orderBy, limit)
+     *
      * @return
      */
-    public Cursor select()
-    {
-        SQLiteDatabase db=this.getReadableDatabase();
-        Cursor cursor=db.query(FeedEntry.TABLE_NAME, null, null, null, null, null,  " _id desc");
+    public Cursor select() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(WORDTABLE.TABLE_NAME, null, null, null, null, null, " _id desc");
         return cursor;
     }
 
-    public void delete(int id)
-    {
-        SQLiteDatabase db=this.getWritableDatabase();
-        String where=FeedEntry._ID+"=?";
-        String[] whereValue={Integer.toString(id)};
-        db.delete(FeedEntry.TABLE_NAME, where, whereValue);
+    /**
+     * sql select *
+     *
+     * @return
+     */
+    public Cursor select(int amount) {
+        String limit = String.valueOf(amount);
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(WORDTABLE.TABLE_NAME, null, null, null, null, null, " _id desc", limit);
+        return cursor;
     }
-    public void delete(String word)
-    {
-        SQLiteDatabase db=this.getWritableDatabase();
-        String where=FeedEntry.COLUMN_NAME_WORD+"=?";
-        String[] whereValue={word};
-        db.delete(FeedEntry.TABLE_NAME, where, whereValue);
+
+    /**
+     * sql select 10
+     *
+     * @return
+     */
+    public Cursor select(int amount, int degree) {
+        String limit = String.valueOf(amount);
+        String[] selectionArgs = {String.valueOf(degree)};
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(WORDTABLE.TABLE_NAME, null, "degree=?", selectionArgs, null, null, " _id desc", limit);
+        return cursor;
+    }
+
+    public void delete(int id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String where = WORDTABLE._ID + "=?";
+        String[] whereValue = {Integer.toString(id)};
+        db.delete(WORDTABLE.TABLE_NAME, where, whereValue);
+    }
+
+    public void delete(String word) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String where = WORDTABLE.COLUMN_NAME_WORD + "=?";
+        String[] whereValue = {word};
+        db.delete(WORDTABLE.TABLE_NAME, where, whereValue);
+    }
+
+    public void update(String word,int degree){
+        String[] whereArgs = {word};
+        ContentValues cv = new ContentValues();
+        cv.put("degree",String.valueOf(degree));
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.update(WORDTABLE.TABLE_NAME, cv, "word=?", whereArgs);
+    }
+
+    /**
+     * user表的操作
+     * @param name
+     * @param time
+     * @param wordmark
+     * @return
+     */
+    public long userInsert(String name, String time, String wordmark) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(USERTABLE.COLUMN_NAME_NAME, name);
+        cv.put(USERTABLE.COLUMN_NAME_TIME, time);
+        cv.put(USERTABLE.COLUMN_NAME_WORDMARK, wordmark);
+        long newRowId;
+        newRowId = db.insert(
+                USERTABLE.TABLE_NAME,
+                null,
+                cv);
+        return newRowId;
+    }
+    public void userUpdate(String wordmark){
+        String[] whereArgs = {"sun"};
+        ContentValues cv = new ContentValues();
+        cv.put("mean",wordmark);
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.update(USERTABLE.TABLE_NAME,cv,"name=?",whereArgs);
+    }
+    public Cursor userSelect() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(USERTABLE.TABLE_NAME, null, null, null, null, null, " _id desc");
+
+        return cursor;
     }
 
 }
